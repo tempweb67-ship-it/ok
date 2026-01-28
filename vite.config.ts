7be2f18fd36574c2595a3a8f07071bceeab56267
rzowais11t <rzowais11@gmail.com>
2026-01-29 01:59:54 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,6 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+    include: ['three', '@react-three/fiber', '@react-three/postprocessing', 'postprocessing'],
   },
 });
