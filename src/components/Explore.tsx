4e9df2ae09511e413018fca5eb18c9788988eb2c
rzowais11t <rzowais11@gmail.com>
2026-01-29 04:39:33 +0530

    Updated Explore.tsx

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -27,7 +27,7 @@ export default function Explore() {
               Explore
             </h1>
             <p className="text-lg text-white text-opacity-60 max-w-2xl mt-6">
-              Discover how we drive digital transformation and unlock endless possibilities for forward-thinking organizations
+              Navigate across the website with ease and look at what we have to offer
             </p>
           </div>
 
