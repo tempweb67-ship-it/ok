c936a527f5d62a672a9b7472b854984b4bdc1c82
rzowais11t <rzowais11@gmail.com>
2026-01-29 18:31:13 +0530

    Updated Solutions.tsx

--- src/components/Solutions.tsx
+++ src/components/Solutions.tsx
@@ -78,7 +78,7 @@ export default function Solutions() {
               <p
                 className="text-sm sm:text-base mt-0.5"
                 style={{
-                  color: '#00ff88',
+                  color: '#1a1a1a',
                   overflow: 'hidden',
                   whiteSpace: 'nowrap',
                   width: 'fit-content',
