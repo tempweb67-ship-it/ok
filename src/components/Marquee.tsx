4fd98b354c95d07a97564e69aa6d60987b952fc9
rzowais11t <rzowais11@gmail.com>
2026-01-29 17:15:32 +0530

    Updated Marquee.tsx

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -2,12 +2,12 @@ import Marquee from 'react-fast-marquee';
 
 export default function BottomMarquee() {
   return (
-    <div className="fixed bottom-0 left-0 w-full z-50 bg-black py-1 border-t border-white border-opacity-10">
+    <div className="fixed bottom-0 left-0 w-full z-50 backdrop-blur-md bg-white/20 py-1 border-t border-black/10">
       <Marquee speed={50} autoFill>
         <span className="text-xs font-bold uppercase tracking-wider mx-4">
-          <span className="text-white">WHERE AI MEETS </span>
-          <span className="text-emerald-400">BUSINESS EXCELLENCE</span>
-          <span className="text-white">.</span>
+          <span className="text-black">WHERE AI MEETS </span>
+          <span className="text-black">BUSINESS EXCELLENCE</span>
+          <span className="text-black">.</span>
         </span>
       </Marquee>
     </div>
