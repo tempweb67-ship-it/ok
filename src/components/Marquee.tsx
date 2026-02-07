00cbb38843e80c22a89bbd641073380fdfdde809
rzowais11t <rzowais11@gmail.com>
2026-02-07 16:59:12 +0530

    Updated Marquee.tsx

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -5,9 +5,9 @@ export default function BottomMarquee() {
     <div className="fixed bottom-0 left-0 w-full z-50 backdrop-blur-md bg-white/10 py-1 border-t border-white/5">
       <Marquee speed={50} autoFill>
         <span className="text-xs font-bold uppercase tracking-wider mx-4">
-          <span className="text-white/60">WHERE AI MEETS </span>
-          <span className="text-white/60">BUSINESS EXCELLENCE</span>
-          <span className="text-white/60">.</span>
+          <span className="text-white">WHERE AI MEETS </span>
+          <span className="text-white">BUSINESS EXCELLENCE</span>
+          <span className="text-white">.</span>
         </span>
       </Marquee>
     </div>
