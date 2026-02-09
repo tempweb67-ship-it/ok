cd6d7d8a75a172349aad478a64145daf3bd2e053
rzowais11t <rzowais11@gmail.com>
2026-02-09 16:17:32 +0530

    Updated Solutions.tsx

--- src/components/Solutions.tsx
+++ src/components/Solutions.tsx
@@ -76,7 +76,7 @@ export default function Solutions() {
                 Built from the ground up on demand.
               </p>
               <p className="text-sm sm:text-base mt-0.5 text-white/40">
-                Six emerging solutions for the year.
+                Six emerging solutions for the year
               </p>
             </div>
           </div>
